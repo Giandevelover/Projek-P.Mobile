76157c6a1bc190db79cff0b2109e7a14f9be4bbe
Az-Rof <rofiazhar002@gmail.com>
2023-05-27 14:46:42 +0700

    Intent Sesi login

--- app/src/main/java/crud/if21f/kostaja/SesiLogin.java
+++ app/src/main/java/crud/if21f/kostaja/SesiLogin.java
@@ -1,7 +1,9 @@
 package crud.if21f.kostaja;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +22,24 @@ public class SesiLogin extends AppCompatActivity {
         user1_sesi = findViewById(R.id.user1_sesi);
         user2_sesi = findViewById(R.id.user2_sesi);
         about_sesi = findViewById(R.id.about_sesi);
+
+        user1_sesi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent(SesiLogin.this, LoginUser.class);
+                //x.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(x);
+            }
+        });
+
+        user2_sesi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent(SesiLogin.this, LoginAdmin.class);
+                //x.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(x);
+            }
+        });
 
     }
 }
