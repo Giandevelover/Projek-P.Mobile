4df3594ccef9e3d6f117879fe50bbf34c097d569
Yusuf0803 <baeyusuf094@gmail.com>
2023-05-27 12:56:57 +0700

    Update terbaru

--- app/src/main/java/crud/if21f/kostaja/SplashScreen.java
+++ app/src/main/java/crud/if21f/kostaja/SplashScreen.java
@@ -1,6 +1,8 @@
 package crud.if21f.kostaja;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreen.this,SesiLogin.class));
+                finish();
+            }
+        }, 900);
     }
 }
