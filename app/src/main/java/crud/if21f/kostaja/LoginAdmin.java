d61701910c3e0950d84cabd528d39bd170a558cc
Yusuf0803 <baeyusuf094@gmail.com>
2023-05-27 11:53:18 +0700

    Update terbaru

--- app/src/main/java/crud/if21f/kostaja/LoginAdmin.java
+++ app/src/main/java/crud/if21f/kostaja/LoginAdmin.java
@@ -1,14 +1,30 @@
 package crud.if21f.kostaja;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginAdmin extends AppCompatActivity {
 
+    private EditText Email_logus, Password_logus;
+    private TextView Daftar_logus, Forget_logus;
+    private Button Btn_Login_logus;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginadmin);
+        Email_logus = findViewById(R.id.Email_admin);
+        Password_logus = findViewById(R.id.Password_admin);
+        Daftar_logus = findViewById(R.id.Daftar_admin);
+        Forget_logus = findViewById(R.id.Forget_admin);
+        Btn_Login_logus = findViewById(R.id.Btn_Login_admin);
+
     }
 }
--- app/src/main/java/crud/if21f/kostaja/LoginUser.java
+++ app/src/main/java/crud/if21f/kostaja/LoginUser.java
@@ -1,14 +1,29 @@
 package crud.if21f.kostaja;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginUser extends AppCompatActivity {
 
+    private EditText Email_user, Password_user;
+    private TextView Daftar_user, Forget_user;
+    private Button Btn_Login_user;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginuser);
+
+        Email_user = findViewById(R.id.Email_user);
+        Password_user = findViewById(R.id.Password_user);
+        Daftar_user = findViewById(R.id.Daftar_user);
+        Forget_user = findViewById(R.id.Forget_user);
+        Btn_Login_user = findViewById(R.id.Btn_Login_user);
     }
 }
